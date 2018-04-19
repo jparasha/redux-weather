9e018d3968be4b9298520a87573ccbf86ec64d4f
jparasha <jayant.parashar@capgemini.com>
2018-04-19 19:02:36 +0530

    fixed import

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,10 +1,13 @@
-import FETCH_WEATHER from '../actions/index';
+import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action){
     console.log('action recieved: ', action);
+    console.log(action.type, FETCH_WEATHER, (action.type == FETCH_WEATHER));
     switch (action.type){
-        case FETCH_WEATHER:
+        case FETCH_WEATHER: {
+            console.log('state', state);
             return [action.payload.data, ...state];
+        }
     }
     return state;
 }
