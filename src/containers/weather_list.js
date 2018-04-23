73658d43449195931fda7a204731ab095fc49462
jparasha <jayant.parashar@capgemini.com>
2018-04-23 14:33:01 +0530

    converted kelvin to celcius

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,13 +6,15 @@ import GoogleMap from '../components/google_map';
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
+        //const temps = cityData.list.map(weather => weather.main.temp);
+        const temps = cityData.list.map(weather => (weather.main.temp-273).toPrecision(2));
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidity = cityData.list.map(weather => weather.main.humidity);
         const  { lon, lat } = cityData.city.coord; //es6
         //const lat = cityData.city.coord.lat;
 
-        console.log('lon', lon);
+        //console.log('temps', temps.map(celcius=>(temps)));
+        console.log('temps', cityData.list.map(weather => (weather.main.temp-273).toPrecision(2)));
         return (
             <tr className="data-row" key={name}>
                 <td><GoogleMap lon={lon} lat ={lat}/></td>
